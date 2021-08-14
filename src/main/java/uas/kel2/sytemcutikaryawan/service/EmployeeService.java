aab32b19067cf1551fa467d86fe1f3c78f515d62
iqbalsihabudin <iqbalsihabudin111@gmail.com>
2021-08-14 12:45:44 +0700

    create package service and create employeeService

---
+++ src/main/java/uas/kel2/sytemcutikaryawan/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package uas.kel2.sytemcutikaryawan.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import uas.kel2.sytemcutikaryawan.models.Employee;
+import uas.kel2.sytemcutikaryawan.repo.EmployeeRepo;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class EmployeeService implements UserDetailsService {
+
+    @Autowired
+    EmployeeRepo employeeRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return employeeRepo.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format("user with email '%s' not found", username)));
+    }
+
+    public Employee registerEmployee(Employee user){
+        boolean userExists = employeeRepo.findByUsername(user.getUsername()).isPresent();
+        if (userExists){
+            throw new RuntimeException(
+                    String.format("user with username '%s' already exists",user.getUsername())
+            );
+        }
+
+        String encondedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encondedPassword);
+        return appUserRepo.save(user);
+    }
+}
