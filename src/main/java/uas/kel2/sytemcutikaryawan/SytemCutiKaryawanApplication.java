e0c095a1ae6564ea5f9c3f6d0ff99e13c53b4da2
iqbalsihabudin <iqbalsihabudin111@gmail.com>
2021-08-14 11:03:38 +0700

    create projek java

---
+++ src/main/java/uas/kel2/sytemcutikaryawan/SytemCutiKaryawanApplication.java
@@ -0,0 +1,13 @@
+package uas.kel2.sytemcutikaryawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SytemCutiKaryawanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SytemCutiKaryawanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uas/kel2/sytemcutikaryawan/SytemCutiKaryawanApplicationTests.java
@@ -0,0 +1,13 @@
+package uas.kel2.sytemcutikaryawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SytemCutiKaryawanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
